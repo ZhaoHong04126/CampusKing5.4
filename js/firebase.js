07fa4afccefa664c06c7bc0cc7d068401205ee5c
Huang Zhaohong <huaug9672@gmail.com>
2026-02-04 19:46:29 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,18 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB23oLjZORs-stYVs-mecqJO5IVyLsTKi4",
-    authDomain: "huaug-4b543.firebaseapp.com",
-    projectId: "huaug-4b543",
-    storageBucket: "huaug-4b543.firebasestorage.app",
-    messagingSenderId: "109106734584",
-    appId: "1:109106734584:web:8fc2e296fc5e22a345a20d",
-    measurementId: "G-LMQ8WDEMGG"
+  apiKey: "AIzaSyB9H2RRxs_7ERb2ZYztuCQu6zScMkOPW_Q",
+  authDomain: "campusmate-4.firebaseapp.com",
+  projectId: "campusmate-4",
+  storageBucket: "campusmate-4.firebasestorage.app",
+  messagingSenderId: "508067016216",
+  appId: "1:508067016216:web:eec789f59fadd8a13d9d6d",
+  measurementId: "G-LMQRW33ML0"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const db = firebase.firestore();
+
 
 
 const ADMIN_UID = "KEshDpx03GQvMNCoHDncJY7SzCF3";
